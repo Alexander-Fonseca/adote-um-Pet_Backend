d4cef039d5126dbf5dd32e7cb632e0fe1d751f67
Alexander Fonseca <motoleco@hotmail.com>
2022-06-02 15:23:42 -0300

    dia1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../ui/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material';
+import tema from '../ui/themes/tema';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <ThemeProvider theme={tema} >
+    <Component {...pageProps} />
+  </ThemeProvider>
+  )
 }
 
 export default MyApp
