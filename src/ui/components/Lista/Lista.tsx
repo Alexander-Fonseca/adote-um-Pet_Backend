a2c312ba6384264afa5b719ce0d7a17b68704647
Alexander Fonseca <motoleco@hotmail.com>
2022-06-03 14:13:23 -0300

    dia1

--- src/ui/components/Lista/Lista.tsx
+++ src/ui/components/Lista/Lista.tsx
@@ -9,21 +9,23 @@ import {
   Descricao
 } from './Lista.style'
 import { Pet } from '../../../data/@types/Pet'
+import { TextService } from '../../../data/services/TextService'
 
 interface ListaProps{
   pets: Pet[];
 }
 
 export default function Lista(props: ListaProps){
+    const tamanhoMaximoTexto = 200;
   return (
     <ListaStyled>
       {props.pets.map(pet =>(
-        <Itemlista>
+        <Itemlista key={pet.id}>
         <Foto src={pet.foto} alt={pet.nome}/>
           <Informacoes>
             <Nome>{pet.nome}</Nome>
             <Descricao>
-              {pet.historia}
+              {TextService.Limitartexto(pet.historia, tamanhoMaximoTexto)}
             </Descricao>
               <Button variant={'contained'}fullWidth>
                 Adotar{pet.nome}
