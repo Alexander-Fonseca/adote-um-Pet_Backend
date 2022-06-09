d9b335a256f86a173005282e13a2542f3e2beee3
Alexander Fonseca <motoleco@hotmail.com>
2022-06-09 15:54:51 -0300

    dia 5

--- src/ui/components/CabecalhoAdmin/CabecalhoAdmin.tsx
+++ src/ui/components/CabecalhoAdmin/CabecalhoAdmin.tsx
@@ -1,3 +1,30 @@
+import { Link, Box } from '@mui/material';
+import NextLink from 'next/Link';
+import { initScriptLoader } from 'next/script';
+
+import {
+  CabecalhoContainer,
+  Logo,
+  LinksContainer
+} from './CabecalhoAdmin.style';
+
 export default function CabecalhoAdmin(){
-  return <div>gdgs</div>
+  return (
+    <CabecalhoContainer>
+      <div>
+        <Logo src={'/imagens/logo.svg'} alt={'Adote um Pet'}/>
+          <LinksContainer>
+            <Link component={NextLink} href={'/pets/cadastro'}>
+              <a>Cadastrar Pet</a> 
+              </Link>
+              <Link component={NextLink} href={'/pets/relatorio'}>
+              <a>Relatório 
+                <Box component={'span'} sx={{display: {sm: 'initial', xs:'none'}}}
+                > de Adoção</Box>
+                </a> 
+              </Link>
+          </LinksContainer>
+      </div>
+    </CabecalhoContainer>
+  )
 }
